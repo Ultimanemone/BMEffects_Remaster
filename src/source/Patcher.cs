b97b7b127f7df17c39ea54213abe96508932b7d3
Ultimanemone <tin210205@gmail.com>
2025-12-07 00:45:43 +0700

    init

---
+++ src/source/Patcher.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using MTMTVFX.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+namespace BMEffects_Remaster
+{
+    [HarmonyPatch(typeof(AssetRegistry), "Init")]
+    public class AssetRegistryPatch
+    {
+        private static void Postfix(AssetRegistry __instance)
+        {
+            AssetRegistry.Register(AssetLoader.GetAllAssets(new Guid(Plugin.guid)), 1, "BMEffects_Remaster");
+        }
+    }
+
+    [HarmonyPatch(typeof(VFXManager), "AddScriptConditional")]
+    public class VFXManagerPatch
+    {
+        private static void Prefix(VFXManager __instance, GameObject obj, string objName, string modName)
+        {
+            if (modName == "BMEffects_Remaster")
+            {
+                string objType = objName.Split('_')[0];
+                if (objType == "muzzleflash" || objType == "expl")
+                {
+                    List<GameObject> smokes = obj.GetComponentsInChildren<Transform>(true)
+                                              .Where(t => t.name.Contains("Smoke"))
+                                              .Select(t => t.gameObject)
+                                              .ToList();
+                    if (smokes != null && smokes.Count > 0)
+                    {
+                        foreach (GameObject smoke in smokes)
+                        {
+                            smoke.AddComponent<SmokeColorer>();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/source/SmokeColorer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using BrilliantSkies.Environments;
+
+namespace BMEffects_Remaster
+{
+    internal class SmokeColorer : MonoBehaviour
+    {
+        private Material mat;
+        private Color color1;
+        private Color color2;
+
+        private void Awake()
+        {
+            mat = GetComponent<ParticleSystem>().GetComponent<ParticleSystemRenderer>().material;
+            color1 = mat.GetColor("_Color1");
+            color2 = mat.GetColor("_Color2");
+        }
+
+        private void Update()
+        {
+            if (mat != null)
+            {
+                float timeMult = Mathf.Cos((FtdEnvironmentManager.Instance.TimeOfDay - 12f) / 24f * Mathf.PI * 2f) * 0.7f + 0.3f;
+                //Color ambientColor = FtdEnvironmentManager.Instance.CurrentWeather.AmbientLightColor * FtdEnvironmentManager.Instance.CurrentWeather.AmbientLightIntensity;
+                //Color sunshaftColor = FtdEnvironmentManager.Instance.CurrentWeather.SunShaftColor * FtdEnvironmentManager.Instance.CurrentWeather.SunShaftIntensity;
+                mat.SetColor("_Color1", color1 * timeMult);
+                mat.SetColor("_Color2", color2 * timeMult);
+            }
+        }
+    }
+}
