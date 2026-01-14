fec30ea4dcb3f2d49ec6f64f0311cb96b48e02e0
Ultimanemone <tin210205@gmail.com>
2026-01-14 18:25:06 +0800

    added steam workshop version checking

---
+++ Source/source/CorePlugin.cs
@@ -0,0 +1,24 @@
+﻿using BrilliantSkies.Modding;
+using HarmonyLib;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace BMEffects_Remaster
+{
+    public class CorePlugin : GamePlugin_PostLoad
+    {
+        public string name { get { return ModInfo.ModName; } }
+        public Version version { get { return ModInfo.Version; } }
+
+        public void OnLoad()
+        {
+            ModInfo.CheckVersion();
+            new Harmony("BMEffects_Remaster").PatchAll();
+        }
+        
+        public void OnSave() { }
+        public bool AfterAllPluginsLoaded() => true;
+    }
+}
